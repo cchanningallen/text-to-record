f130ee746839aa07bc0acbc0eff10dfbb903c3b8
cchanningallen <cchanningallen@gradle.com>
2021-02-15 10:11:50 -0500

    [Twilio] Debug: Echo SMS body

--- src/pages/api/twilio-sms.js
+++ src/pages/api/twilio-sms.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 async function twilioSMS(req, res) {
     console.log(req.body);
 
-    res.status(200).json({ hello: 'world!' });
+    res.status(200).json({ hello: 'world!', reqBody: req.body });
 }
 
 exports.twilioSMS = twilioSMS;
