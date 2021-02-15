82d7070abefdb70d8eabde99b93c1254f7e57548
cchanningallen <cchanningallen@gradle.com>
2021-02-15 10:01:02 -0500

    Stand up twilio endpoint to test connection, finish wiring hasura record creation

---
+++ src/pages/api/create-record.js
@@ -0,0 +1,30 @@
+const { hasuraRequest } = require('./util/hasura');
+
+async function createRecord(req, res) {
+    const { title, text, raw, type } = JSON.parse(req.body);
+
+    const variables = {
+        title,
+        text,
+        raw,
+    };
+    if (type) {
+        variables.type = type;
+    }
+
+    const data = await hasuraRequest({
+        query: `
+            mutation CreateRecord($text: String!, $title: String!, $raw: String!, $type: String) {
+                insert_textRecords_one(object: {title: $title, text: $text, raw: $raw, type: $type}) {
+                    id
+                }
+            }
+        `,
+        variables,
+    });
+
+    res.status(200).json({ data });
+}
+
+exports.createRecord = createRecord;
+export default createRecord;
